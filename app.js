cb1767a840dd193589064eee761b34899e9c69d7
Lukasz-Milde <lukasz@milde.live>
2023-01-14 19:41:15 +0100

    pierwsza sobota

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 console.log("jazda!");
 
-const one = document.querySelector(".one");
+const one = document.querySelector(".one-grid");
+const oneMove = document.querySelector(".one--move");
 
 one.addEventListener("mouseenter", () => {
-	one.classList.add("moveOne");
+	console.log("hover dziala");
+	oneMove.classList.add("one--Move__hover");
 });
 one.addEventListener("mouseleave", () => {
-	one.classList.remove("moveOne");
+	oneMove.style.transition = "opacity: 0, transform(30px, 30px)";
 });
