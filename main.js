edec2500523a4a006c8f31b87990d09ff8b4124e
Lukasz-Milde <lukasz@milde.live>
2023-04-17 22:22:04 +0200

    first step of email validation

--- main.js
+++ main.js
@@ -1,68 +1,51 @@
-const form = document.getElementById("contact-form");
-const nameInput = document.getElementById("name");
-const emailInput = document.getElementById("email");
-const subjectInput = document.getElementById("subject");
-const messageInput = document.getElementById("message");
+const form = document.querySelector(".form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const subjectInput = document.querySelector("#subject");
+const messageInput = document.querySelector("#message");
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", (event) => {
 	event.preventDefault();
-	if (validateInputs()) {
-		sendEmail();
+
+	const nameValue = nameInput.value.trim();
+	const emailValue = emailInput.value.trim();
+	const subjectValue = subjectInput.value.trim();
+	const messageValue = messageInput.value.trim();
+
+	if (nameValue === "") {
+		alert("Please enter your name.");
+		nameInput.focus();
+		return;
 	}
+
+	if (emailValue === "") {
+		alert("Please enter your email address.");
+		emailInput.focus();
+		return;
+	}
+
+	if (!isValidEmail(emailValue)) {
+		alert("Please enter a valid email address.");
+		emailInput.focus();
+		return;
+	}
+
+	if (subjectValue === "") {
+		alert("Please enter a subject.");
+		subjectInput.focus();
+		return;
+	}
+
+	if (messageValue === "") {
+		alert("Please enter a message.");
+		messageInput.focus();
+		return;
+	}
+
+	form.submit();
 });
 
-function validateInputs() {
-	let isValid = true;
-
-	if (nameInput.value.trim() === "") {
-		isValid = false;
-		setErrorFor(nameInput, "Name cannot be blank");
-	} else {
-		setSuccessFor(nameInput);
-	}
-
-	if (emailInput.value.trim() === "") {
-		isValid = false;
-		setErrorFor(emailInput, "Email cannot be blank");
-	} else if (!isEmailValid(emailInput.value.trim())) {
-		isValid = false;
-		setErrorFor(emailInput, "Email is not valid");
-	} else {
-		setSuccessFor(emailInput);
-	}
-
-	if (subjectInput.value.trim() === "") {
-		isValid = false;
-		setErrorFor(subjectInput, "Subject cannot be blank");
-	} else {
-		setSuccessFor(subjectInput);
-	}
-
-	if (messageInput.value.trim() === "") {
-		isValid = false;
-		setErrorFor(messageInput, "Message cannot be blank");
-	} else {
-		setSuccessFor(messageInput);
-	}
-
-	return isValid;
-}
-
-function setErrorFor(input, message) {
-	const formControl = input.parentElement;
-	const error = formControl.querySelector(".error-message");
-	error.innerText = message;
-	formControl.className = "form-control error";
-}
-
-function setSuccessFor(input) {
-	const formControl = input.parentElement;
-	formControl.className = "form-control success";
-}
-
-function isEmailValid(email) {
+function isValidEmail(email) {
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	return emailRegex.test(email);
 }
-
-function sendEmail() {}
