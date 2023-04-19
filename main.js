9548be1b0e905bf1a599dde895cc6be8bb089fed
Lukasz-Milde <lukasz@milde.live>
2023-04-19 14:17:52 +0200

    invalid input style

--- main.js
+++ main.js
@@ -14,35 +14,40 @@ form.addEventListener("submit", (event) => {
 
 	if (nameValue === "") {
 		nameInput.value = "Please enter your name";
+		nameInput.parentNode.classList.add("invalidInput");
 		nameInput.focus();
 		return;
 	}
 
 	if (emailValue === "") {
 		emailInput.value = "Please enter your email address";
+		emailInput.parentNode.classList.add("invalidInput");
 		emailInput.focus();
 		return;
 	}
 
 	if (!isValidEmail(emailValue)) {
 		emailInput.value = "Please enter a valid email address";
+		emailInput.parentNode.classList.add("invalidInput");
 		emailInput.focus();
 		return;
 	}
 
 	if (subjectValue === "") {
 		subjectInput.value = "Please enter a subject";
+		subjectInput.parentNode.classList.add("invalidInput");
 		subjectInput.focus();
 		return;
 	}
 
 	if (messageValue === "") {
 		messageInput.value = "Please enter a message";
+		messageInput.parentNode.classList.add("invalidInput");
 		messageInput.focus();
 		return;
 	}
 
-	form.submit();
+	// form.submit();
 });
 
 function isValidEmail(email) {
